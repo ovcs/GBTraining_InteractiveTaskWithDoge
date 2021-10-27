bfbeb7c3a40489c6b7c7be400f0818a9746ed7f5
OvcS <gerkules007@yahoo.com>
2021-10-27 23:16:08 +0300

    Add interactive

--- Programm001/Program.cs
+++ Programm001/Program.cs
@@ -1,21 +1,35 @@
-﻿double distance0 = 10;
+﻿Console.WriteLine("3");
+System.Threading.Thread.Sleep(1000);
+Console.WriteLine("2");
+System.Threading.Thread.Sleep(1000);
+Console.WriteLine("1");
+System.Threading.Thread.Sleep(1000);
+Console.WriteLine("Старт");
+Console.WriteLine(" ");
+
+double distance0 = 10;
 double firstFriendSpeed = 1;
 double secondFriendSpeed = 2;
 double dogSpeed = 5;
-double distance = 10000; 
+double distance = 10000;
 double time;
 int count = 0, friend = 2;  // мы бежим ко второму другу
 
 while (distance > distance0)
 {
+    System.Threading.Thread.Sleep(500);
     if (friend == 2)
     {
         time = distance / (secondFriendSpeed + dogSpeed);
         friend = 1;
     }
-    time = distance / (firstFriendSpeed + dogSpeed);
-    friend = 2;
+    else
+    {
+        time = distance / (firstFriendSpeed + dogSpeed);
+        friend = 2;
+    }
     distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+    Console.Write($"Собачка пробежала {count + 1} раз  |  ");
+    Console.WriteLine($"Расстояние: {distance}");
     count++;
 }
-Console.WriteLine(count);
